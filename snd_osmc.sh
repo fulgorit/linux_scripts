42f222cba3e9c21c36b83c30a91074fd39ceb1a8
fulgorit <l.vanwinnendaele@gmail.com>
2017-08-30 20:10:23 +0200

    Delete snd_osmc.sh

--- snd_osmc.sh
+++
@@ -1,5 +0,0 @@
-pactl load-module module-tunnel-sink "server=192.168.1.55 sink=alsa_output.0.analog-stereo sink_name=osmc"
-#pactl load-module module-combine-sink sink_name=combined slaves=alsa_output.pci-0000_00_1b.0.analog-stereo,osmc
-#pactl load-module module-tunnel-sink "server=192.168.1.40 sink=alsa_output.pci-0000_00_1b.0.analog-stereo sink_name=cubi"
-#pactl load-module module-combine-sink sink_name=combined slaves=alsa_output.pci-0000_00_1b.0.analog-stereo,cubi
-#alsa_output.pci-0000_00_1b.0.analog-stereo
